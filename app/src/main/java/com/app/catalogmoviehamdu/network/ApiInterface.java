fd8e1c8ba62c71cb466597780a3fffcb337ce8f4
hamdu <amdualrozak01@gmail.com>
2020-08-01 09:51:23 +0700

    first commit

---
+++ app/src/main/java/com/app/catalogmoviehamdu/network/ApiInterface.java
@@ -0,0 +1,20 @@
+package com.app.catalogmoviehamdu.network;
+
+import com.app.catalogmoviehamdu.model.Movie;
+import com.app.catalogmoviehamdu.util.Constant;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiInterface {
+
+    @GET(Constant.MOVIE_PATH + "/popular")
+    Call<Movie> popularMovies(
+            @Query("page") int page);
+
+    @GET(Constant.MOVIE_PATH +"/{movie_id}/similar")
+    Call<Movie> similarMovies(
+            @Query("page") int page);
+
+}
